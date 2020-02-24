621d84631adb7e2dbbfc4ab2f274985aad2bd7fa
Combii <davidstovle@gmail.com>
2020-02-24 11:55:48 +0100

    Added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MenuComponent } from './menu/menu.component';
 import { TableDataComponent } from './table-data/table-data.component';
+import { TableDataExcelComponent } from './table-data-excel/table-data-excel.component';
 
 
 const routes: Routes = [
   {path: '', component: MenuComponent},
-  {path: 'table', component: TableDataComponent}
+  {path: 'table', component: TableDataComponent},
+  {path: 'tableEx', component: TableDataExcelComponent}
 ];
 
 @NgModule({
