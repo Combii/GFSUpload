78e38de871573259bcfb03cba8343d7b9dd9117f
Combii <davidstovle@gmail.com>
2020-02-24 11:44:50 +0100

    Somewhat working menu with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MenuComponent } from './menu/menu.component';
+import { TableDataComponent } from './table-data/table-data.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MenuComponent},
+  {path: 'table', component: TableDataComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
