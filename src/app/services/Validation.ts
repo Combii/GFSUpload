221739fb7a864b29f1fa4edc7e6d0dd237f9e62a
Combii <davidstovle@gmail.com>
2020-02-25 14:07:14 +0100

    Fixed date input

--- src/app/services/Validation.ts
+++ src/app/services/Validation.ts
@@ -5,15 +5,22 @@ export class Validations {
     if (!/^(\d){8}$/.test(date)) { return false; }
 
     const year = Number(date.toString().substring(0, 4));
-    const month = Number(date.toString().substring(4, 2));
-    const day = Number(date.toString().substring(6, 2));
+    const month = Number(date.toString().substring(4, 6));
+    const day = Number(date.toString().substring(6, 8));
+
+    // console.log('Year ' + year);
+    // console.log('Month ' + month);
+    // console.log('Day ' + day);
+
 
     const parsedDate = new Date(year, month, day);
 
     // Check if sunday or saturday
     const dayOfWeek = parsedDate.getDay();
 
-    if (dayOfWeek === 0 || dayOfWeek === 6) {
+    // Saturday === 0
+    // Sunday === 1
+    if (dayOfWeek === 0 || dayOfWeek === 1) {
         return false;
     }
 
