4c284e59bffa9fdda4713fa779e8f8be646a690c
Combii <davidstovle@gmail.com>
2020-02-25 13:58:57 +0100

    added fix for substring not a function

--- src/app/services/Validation.ts
+++ src/app/services/Validation.ts
@@ -4,10 +4,10 @@ export class Validations {
     // https://stackoverflow.com/questions/10638529/how-to-parse-a-date-in-format-yyyymmdd-in-javascript
     if (!/^(\d){8}$/.test(date)) { return false; }
 
-    // tslint:disable-next-line: one-variable-per-declaration
-    const year = Number(date.substr(0, 4)),
-    month = Number(date.substr(4, 2)),
-    day = Number(date.substr(6, 2));
+    const year = Number(date.toString().substring(0, 4));
+    const month = Number(date.toString().substring(4, 2));
+    const day = Number(date.toString().substring(6, 2));
+
     const parsedDate = new Date(year, month, day);
 
     // Check if sunday or saturday
