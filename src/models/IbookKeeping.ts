60dd4595e06e403850db5f4f4ad5f43d2c40d077
Combii <davidstovle@gmail.com>
2020-02-26 14:05:03 +0100

    added new interface

--- src/models/IbookKeeping.ts
+++ src/models/IbookKeeping.ts
@@ -1,6 +1,21 @@
-interface Error {
-  index: number;
-  errorMessage: string;
+export interface IBookKeepingError {
+  Dato: string[];
+  RegNr: string[];
+  regnskabstype: string[];
+  dkkbass: string[];
+  skema_id: string[];
+  skemarakke: string[];
+  valutakod: string[];
+  ldkd: string[];
+  kngr: string[];
+  kngr_typ: string[];
+  pdst: string[];
+  sum_rgopid: string[];
+  opdater_lev: string[];
+  leveran_kor: string[];
+  leveran_type: string[];
+  saldo: string[];
+  Tekst: string[];
 }
 
 export interface IBookKeeping {
