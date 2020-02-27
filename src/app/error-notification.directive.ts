09692fd7c86597c7820b18981d1a8bb960e49b27
Combii <davidstovle@gmail.com>
2020-02-27 12:21:49 +0100

    Almost working

--- src/app/error-notification.directive.ts
+++ src/app/error-notification.directive.ts
@@ -1,17 +1,18 @@
-import { Directive, ElementRef, Input } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit, HostBinding } from '@angular/core';
 import { IBookKeeping } from 'src/models/IbookKeeping';
 
 @Directive({
   selector: '[appErrorNotification]'
 })
-export class ErrorNotificationDirective {
-
+export class ErrorNotificationDirective implements OnInit {
   @Input('appErrorNotification') errorsArray: string[];
-  
-  constructor(private el: ElementRef) {
+  @HostBinding('style.backgroundColor') backgroundColor: string;
+
+  constructor(private elRef: ElementRef) {}
+
+  ngOnInit(): void {
     console.log(this.errorsArray);
-   }
+  }
 
+  
 }
-
-//AccountingDate IDKT OriginalIDKT Text
