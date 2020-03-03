319b50112e76a468af1cad1051ef0eb01374bc12
Combii <davidstovle@gmail.com>
2020-03-03 14:50:42 +0100

    removed papaparse as it is not needed

--- src/app/table-data/table-data.component.ts
+++ src/app/table-data/table-data.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import * as Papa from 'papaparse';
 import { IBookKeeping } from '../../models/IbookKeeping';
-import { CSVParserService } from '../services/CSVParser.service';
 import { ExcelParserService } from '../services/excelParser.service';
 
 @Component({
@@ -13,7 +12,6 @@ export class TableDataComponent {
   dataList: IBookKeeping[] = [];
 
   constructor(
-    private csvParseService: CSVParserService,
     private excelparser: ExcelParserService
   ) {}
 
@@ -22,15 +20,6 @@ export class TableDataComponent {
     this.excelparser.onExcelFileParsedIBookKeeping.subscribe(rData => {
       this.dataList = rData;
     });
-  }
-
-  // Reads csv file
-  onChange(files: File[]) {
-    const ext = this.getExtension(files[0].name);
-    console.log(ext);
-
-    this.csvParseService.parseCSV(files);
-    this.dataList = this.csvParseService.dataList;
   }
 
   getExtension(filename) {
