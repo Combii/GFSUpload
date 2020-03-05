4b92dc1da1f31214e7f112866c02279c989459b0
Combii <davidstovle@gmail.com>
2020-03-05 14:56:08 +0100

    Small comment

--- src/app/services/Validation.spec.ts
+++ src/app/services/Validation.spec.ts
@@ -1,9 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { Validations } from './Validation';
 
-
+// Run ng test
 describe('ValidationTest', () => {
-
 
     it('ValidateCurrency', () => {
     const result = Validations.IsValidCurrency('DKKK')
