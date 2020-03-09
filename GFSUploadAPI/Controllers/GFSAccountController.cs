94cc9b66fdda3f44f386e5a221a0cd9003ba7d67
Combii <davidstovle@gmail.com>
2020-03-09 23:24:12 +0100

    Update GFSAccountController.cs

--- GFSUploadAPI/Controllers/GFSAccountController.cs
+++ GFSUploadAPI/Controllers/GFSAccountController.cs
@@ -17,16 +17,9 @@ namespace GFSUploadAPI.Controllers
         }
 
         [HttpPost]
-        public void Post()
+        public void Post(AccountBookKeeping accountBookKeeping)
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new BookKeeping
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            Console.WriteLine(accountBookKeeping);
         }
     }
 }
