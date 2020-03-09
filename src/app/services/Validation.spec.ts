a30e6ca29ad9e048bde352d0446d171f5afff13f
Combii <davidstovle@gmail.com>
2020-03-09 13:48:30 +0100

    Added more testing

--- src/app/services/Validation.spec.ts
+++ src/app/services/Validation.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { Validations } from './Validation';
 import { CheckboxService } from './checkbox.service';
+import { error } from 'protractor';
 
 // Run ng test
 describe('ValidationTest', () => {
@@ -11,9 +12,78 @@ describe('ValidationTest', () => {
     expect(result[0]).toEqual('Currency is not three chars long');
     });
 
-    it('ValidateDate', () => {
-        const result = Validations.IsValidDate('', new CheckboxService[])
+    it('ValidateDate Sunday or Saturday', () => {
 
-        expect(result[0]).toEqual('Currency is not three chars long');
+        const checkboxService = new CheckboxService();
+
+        checkboxService.bookInFebos = true;
+        checkboxService.bookInFebosAndUploadToGfs = true;
+        checkboxService.uploadToGfs = true;
+
+        const result = Validations.IsValidDate('20200307', checkboxService);
+
+        console.log(result)
+
+        let isValid = false;
+
+        result.forEach(err => {
+            if(err === 'Date cannot be saturday or sunday'){
+                isValid = true;
+            }
         });
+
+        expect(isValid).toEqual(true);
+    });
+
+    it('ValidateDate first of January', () => {
+
+        const result = Validations.IsValidDate('20200101');
+
+        console.log(result)
+
+
+        let isValid = false;
+
+        result.forEach(err => {
+            if(err === 'Date cannot be first of January'){
+                isValid = true;
+            }
+        });
+
+        expect(isValid).toEqual(true);
+    });
+
+    it('ValidateDate first of January V2', () => {
+
+        const result = Validations.IsValidDate('20190101');
+
+        console.log(result)
+
+        let isValid = false;
+
+        result.forEach(err => {
+            if(err === 'Date cannot be first of January'){
+                isValid = true;
+            }
+        });
+
+        expect(isValid).toEqual(true);
+    });
+
+    it('ValidateDate first of January V3', () => {
+
+        const result = Validations.IsValidDate('20070101');
+
+        console.log(result)
+
+        let isValid = false;
+
+        result.forEach(err => {
+            if(err === 'Date cannot be first of January'){
+                isValid = true;
+            }
+        });
+
+        expect(isValid).toEqual(true);
+    });
 });
