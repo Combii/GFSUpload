3eec054b1a4115572a9c2e8acba12741566c4c5c
Combii <davidstovle@gmail.com>
2020-03-12 19:21:44 +0100

    Added for booking date added

--- GFSUploadAPI/Models/BookKeeping.cs
+++ GFSUploadAPI/Models/BookKeeping.cs
@@ -5,8 +5,15 @@
     {
         public class BookKeeping
         {
+
+          public BookKeeping()
+          {
+            CreatedDate  = DateTime.Now;
+          }
+
           [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
           public int Id { get; set; }
+          public DateTime CreatedDate { get; }
           public string Dato { get; set; }
           public string RegNr { get; set; }
           public string regnskabstype { get; set; }
