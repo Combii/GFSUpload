73531557e7f735a99815e85b1c72e86247ea9b6c
borisgrunwald <borisgrunwald@gmail.com>
2020-03-12 17:54:09 +0100

    Added basic logic for signin and signout

--- GFSUploadAngular/src/app/auth/auth/auth.component.ts
+++ GFSUploadAngular/src/app/auth/auth/auth.component.ts
@@ -1,19 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-auth',
-  templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.css']
+  selector: "app-auth",
+  templateUrl: "./auth.component.html",
+  styleUrls: ["./auth.component.css"]
 })
 export class AuthComponent implements OnInit {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onSignUp(email: string, password: string) {
+/*     this.http.post("https://", {
+      email,
+      password
+    }); */
   }
 
-  onSwitchMode() {
-
+  onSignIn(email: string, password: string) {
+    /*  this.http.post("https://", {
+      email,
+      password
+    }); */
   }
 
+  onSwitchMode() {}
 }
