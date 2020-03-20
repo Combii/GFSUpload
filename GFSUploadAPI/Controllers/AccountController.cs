b24ef1080a109ae2915d878865d470ef3bcf0a00
Combii <davidstovle@gmail.com>
2020-03-20 11:15:04 +0100

    jwt token not done

--- GFSUploadAPI/Controllers/AccountController.cs
+++ GFSUploadAPI/Controllers/AccountController.cs
@@ -1,9 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using GFSUploadAPI.Dtos;
 using GFSUploadAPI.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
 
 namespace GFSUploadAPI.Controllers
 {
@@ -11,12 +19,14 @@ namespace GFSUploadAPI.Controllers
   [Route("api/[controller]")]
   public class AccountController : Controller
   {
+    private readonly IConfiguration _config;
     private readonly UserManager<IdentityUser> _userManager;
     private readonly SignInManager<IdentityUser> _signInManager;
 
-    public AccountController(UserManager<IdentityUser> userManager,
+    public AccountController(IConfiguration config, UserManager<IdentityUser> userManager,
       SignInManager<IdentityUser> signInManager)
     {
+      _config = config;
       _userManager = userManager;
       _signInManager = signInManager;
     }
@@ -35,8 +45,43 @@ namespace GFSUploadAPI.Controllers
 
       return Ok(new
       {
+        token = GenerateJwtToken(userToCreate).Result,
         user = result
       });
+    }
+
+    private async Task<string> GenerateJwtToken(IdentityUser user)
+    {
+      var claims = new List<Claim>
+      {
+        new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+        new Claim(ClaimTypes.Name, user.UserName)
+      };
+
+      var roles = await _userManager.GetRolesAsync(user);
+
+      foreach (var role in roles)
+      {
+        claims.Add(new Claim(ClaimTypes.Role, role));
+      }
+
+      var key = new SymmetricSecurityKey(Encoding.UTF8
+        .GetBytes(_config.GetSection("AppSettings:Token").Value));
+
+      var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+      var tokenDescriptor = new SecurityTokenDescriptor
+      {
+        Subject = new ClaimsIdentity(claims),
+        Expires = DateTime.Now.AddDays(1),
+        SigningCredentials = creds
+      };
+
+      var tokenHandler = new JwtSecurityTokenHandler();
+
+      var token = tokenHandler.CreateToken(tokenDescriptor);
+
+      return tokenHandler.WriteToken(token);
     }
   }
 }
