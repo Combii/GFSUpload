b228017ecd4136ebb266295f5b7ecd7b9d8cf918
borisgrunwald <borisgrunwald@gmail.com>
2020-04-29 17:39:56 +0200

    Added route guards

--- GFSUploadAngular/src/app/app-routing.module.ts
+++ GFSUploadAngular/src/app/app-routing.module.ts
@@ -4,12 +4,13 @@ import { MenuComponent } from './menu/menu.component';
 import { TableDataChartGfsComponent } from './table-data-chart-gfs/table-data-chart-gfs.component';
 import { TableDataAccountComponent } from './table-data-account/table-data-account.component';
 import { AuthComponent } from './auth/auth/auth.component';
+import { AuthGuard } from './auth/auth/auth.guard';
 
 
 const routes: Routes = [
   {path: '', component: MenuComponent},
-  {path: 'tableChartGFS', component: TableDataChartGfsComponent},
-  {path: 'tableAccount', component: TableDataAccountComponent},
+  {path: 'tableChartGFS', canActivate:[AuthGuard] ,component: TableDataChartGfsComponent},
+  {path: 'tableAccount', canActivate:[AuthGuard] ,component: TableDataAccountComponent},
   {path: 'auth', component: AuthComponent}
 ];
 
