2bafff2f39bf897ad4aabea771d7041b1786e59a
Combii <davidstovle@gmail.com>
2020-05-13 17:34:38 +0200

    almost working

--- GFSUploadAPI/Services/AccountBookKeepingToFileParser.cs
+++ GFSUploadAPI/Services/AccountBookKeepingToFileParser.cs
@@ -11,9 +11,7 @@ namespace GFSUploadAPI.Services
             using var sw = new StreamWriter("hostDataFileAccountBookKeepings.txt");
             foreach (var accountBookKeeping in accountBookKeepings)
             {
-                     var row = string.Format("{0,-47}{1,5}{2,14}{3,22}{4,8}{5,24}{6,13}{7,7}",
-                        accountBookKeeping.,"11EUR","LV","00","FEBLOVAPA01BJKPL","1643575,32LATVIA", "JANUARY 2020", "UPLOAD");
-                sw.WriteLine(row;
+                sw.WriteLine(accountBookKeeping.ToString());
             }
         }
 
@@ -22,7 +20,13 @@ namespace GFSUploadAPI.Services
             using var sw = new StreamWriter("hostDataFileBookKeepings.txt");
             foreach (var bookKeeping in bookKeepings)
             {
-                sw.WriteLine(bookKeeping.ToString());
+                var row = string.Format("{0,-47}{1,5}{2,14}{3,22}{4,8}{5,24}{6,13}{7,7}",
+                bookKeeping.CreatedDate + bookKeeping.RegNr + bookKeeping.regnskabstype + bookKeeping.dkkbass + bookKeeping.skema_id,
+                bookKeeping.skemarakke + bookKeeping.valutakod, bookKeeping.ldkd, bookKeeping.kngr, 
+                bookKeeping.kngr_typ + bookKeeping.pdst + bookKeeping.sum_rgopid + bookKeeping.opdater_lev + bookKeeping.leveran_kor + bookKeeping.leveran_type, 
+                bookKeeping.saldo + "LATVIA", "JANUARY 2020", "UPLOAD");
+
+                sw.WriteLine(row);
             }
         }
     }
