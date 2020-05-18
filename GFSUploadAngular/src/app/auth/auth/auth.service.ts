c122285f627390d1cb4eb8b776a02e8582c6d993
Combii <davidstovle@gmail.com>
2020-05-18 15:34:27 +0200

    Update auth.service.ts

--- GFSUploadAngular/src/app/auth/auth/auth.service.ts
+++ GFSUploadAngular/src/app/auth/auth/auth.service.ts
@@ -1,9 +1,9 @@
-import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { NgForm } from "@angular/forms";
-import { Subject, BehaviorSubject } from "rxjs";
-import { tap } from "rxjs/operators";
-import { User } from "./user.model";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { NgForm } from '@angular/forms';
+import { Subject, BehaviorSubject } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { User } from './user.model';
 
 export interface AuthResponseData {
   token: string;
@@ -13,7 +13,7 @@ export interface AuthResponseData {
   };
 }
 
-@Injectable({ providedIn: "root" })
+@Injectable({ providedIn: 'root' })
 export class AuthService {
   user = new BehaviorSubject<User>(null);
 
@@ -21,20 +21,32 @@ export class AuthService {
 
   signup(username: string, password: string) {
     return this.http
-      .post<AuthResponseData>("http://localhost:5000/api/auth/register", {
+      .post<AuthResponseData>('http://localhost:5000/api/auth/register', {
         Username: username,
         Password: password
       })
       .pipe(
         tap(resData => {
-          //this.token.next(resData.token)
+          this.user.next(
+            const expirationDate = new Date(
+              new Date().setDate(new Date().getDate() + 1)
+            );
+
+            new User(
+              resData.user.username,
+              resData.user.id,
+              resData.token,
+              expirationDate
+            )
+          );
+          // this.token.next(resData.token)
         })
       );
   }
 
   login(username: string, password: string) {
     return this.http
-      .post<AuthResponseData>("http://localhost:5000/api/auth/login", {
+      .post<AuthResponseData>('http://localhost:5000/api/auth/login', {
         Username: username,
         Password: password
       })
