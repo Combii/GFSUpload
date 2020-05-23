aeaaa7aa6c6b87fe195ad1f37e1ca5545711e8b0
Combii <davidstovle@gmail.com>
2020-05-23 13:38:07 +0200

    Added password requirements

--- GFSUploadAPI/Startup.cs
+++ GFSUploadAPI/Startup.cs
@@ -49,6 +49,14 @@ namespace GFSUploadAPI
             services.AddIdentity<IdentityUser, IdentityRole>()
               .AddEntityFrameworkStores<DataContext>();
 
+            IdentityBuilder builder = services.AddIdentityCore<IdentityUser>(opt =>
+            {
+                opt.Password.RequireDigit = false;
+                opt.Password.RequiredLength = 4;
+                opt.Password.RequireNonAlphanumeric = false;
+                opt.Password.RequireUppercase = false;
+            });
+
              var key = System.Text.Encoding.UTF8.GetBytes(Configuration["AppSettings:Token"].ToString());
 
             services.AddAuthentication(x =>
